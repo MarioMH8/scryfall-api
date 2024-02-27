347f2ad0a5e740e34f8c7cc1db66af129d7f41a1
MarioMH8 <mhidalgomario@gmail.com>
2024-02-27 15:27:55 +0100

    chore: update entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-export { default as CardIdentifierBuilder } from './api/CardIdentifierBuilder';
-export { default as Cards } from './api/Cards';
-export { default as Catalog } from './api/Catalog';
-export { default as Rulings } from './api/Rulings';
-export { default as Sets } from './api/Sets';
-export { default as Symbology } from './api/Symbology';
+export * from './api';
 export * from './error';
 export { default as MagicPageResult } from './fetcher/page.result';
-export * from './types.old';
